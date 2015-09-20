b794939ab0162a9afa9972cccb27ad658707b711
rafal.lizon@telemabk.pl <rafal.lizon@telemabk.pl>
2015-09-20 12:55:22 +0200

    rep nav

--- js/searchFieldAnimation.js
+++ js/searchFieldAnimation.js
@@ -2,11 +2,11 @@
  * Created by Rafal on 2015-09-19.
  */
 
-$('.searchField button').mouseover(function() {
+$('.searchField input').click(function() {
     $('.second-level .searchField input').animate({width: '450px'});
     $('.second-level .searchField select').css('visibility','visible');
 });
 $('.navbar').mouseleave(function() {
-    $('.second-level .searchField input').animate({width: '0px'});
+    $('.second-level .searchField input').animate({width: '130px'});
     $('.second-level .searchField select').css('visibility','hidden');
 });
