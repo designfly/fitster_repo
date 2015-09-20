2a8b518ba240a3b5427fefe111b7b9957a7c4ba8
designfly <kontakt@designfly.pl>
2015-09-20 20:43:15 +0200

    header

--- page.php
+++ page.php
@@ -1,9 +1,8 @@
 <?php get_header(); ?>
-<div class="page-margin">
+
                 <?php while (have_posts()) : the_post(); ?>
 
                         <?php the_content(); ?>
 
                     <?php endwhile; ?>
-</div>
 <?php get_footer(); ?>
