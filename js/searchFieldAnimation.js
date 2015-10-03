295ccc0d78f995ab579788a9487800995e85916e
rafal.lizon@telemabk.pl <rafal.lizon@telemabk.pl>
2015-10-03 12:00:03 +0200

    repair nav

--- js/searchFieldAnimation.js
+++ js/searchFieldAnimation.js
@@ -4,13 +4,15 @@
 
 
 jQuery.noConflict();
-$('.searchField input').click(function() {
-    $('.second-level .searchField input').animate({width: '450px'});
-    $('.second-level .searchField select').css('visibility','visible');
-});
-jQuery.noConflict();
+jQuery(function($) {
+    $('.searchField input').click(function() {
+        $('.second-level .searchField input').animate({width: '450px'});
+        $('.second-level .searchField select').css('visibility','visible');
+    })
+
 $('.navbar').mouseleave(function() {
     $('.second-level .searchField input').animate({width: '130px'});
     $('.second-level .searchField select').css('visibility','hidden');
+})
 });
-jQuery.noConflict();
+
