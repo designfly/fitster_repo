fbb26ed41a04717a185702779d27d75e04a92bdc
rafal.lizon@telemabk.pl <rafal.lizon@telemabk.pl>
2015-10-03 16:11:55 +0200

    rep gall

--- js/facebook_like_share.js
+++ js/facebook_like_share.js
@@ -12,8 +12,8 @@
 
 jQuery(function($) {
 
-        $(".fancybox").mouseover(function() {console.log('xd'); $(".fb_iframe_widget").css('visibility','visible'); });
+        $(".fancybox").mouseover(function() {console.log('xd'); $(".fb-like").css('visibility','visible'); });
         //$(".fb_iframe_widget").mouseover(function() { $(".fb_iframe_widget").css('visibility','visible'); });
-        $(".fancybox").mouseout(function() {console.log('lol'); $(".fb_iframe_widget").css('visibility','hidden'); });
+        $(".fancybox").mouseout(function() {console.log('lol'); $(".fb-like").css('visibility','hidden'); });
 });
 
